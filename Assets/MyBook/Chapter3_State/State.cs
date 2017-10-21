2a0e63839a546f5d69b51ced60d8a5103f1239c1
RainbowMin <123438952@qq.com>
2017-10-21 21:28:05 +0800

    ttt

---
+++ Assets/MyBook/Chapter3_State/State.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace mj
+{
+    public class Context
+    {
+        private State m_State = null;
+
+        public void SetState(State state)
+        {
+            m_State = state;
+        }
+
+        public void Request(int value)
+        {
+            m_State.Handle(value);
+        }
+    }
+
+    public abstract class State
+    {
+        protected Context m_Context = null;
+
+        public State(Context context)
+        {
+            m_Context = context;
+        }
+
+        public abstract void Handle(int value);
+    }
+
+    public class ConcreteStateA : State
+    {
+        public ConcreteStateA(Context context) : base(context)
+        {
+
+        }
+
+        public override void Handle(int value)
+        {
+            Debug.Log("ConcreteStateA.Handle(), value="+value);
+
+            if(value > 10)
+            {
+                Debug.Log("ConcreteStateA.Handle(), Change To StateB");
+                m_Context.SetState(new ConcreteStateB(m_Context));
+            }
+        }
+    }
+
+    public class ConcreteStateB : State
+    {
+        public ConcreteStateB(Context context) : base(context)
+        {
+        }
+
+        public override void Handle(int value)
+        {
+            Debug.Log("ConcreteStateB.Handle(), value=" + value);
+
+            if (value > 20)
+            {
+                Debug.Log("ConcreteStateB.Handle(), Change To StateC");
+                m_Context.SetState(new ConcreteStateC(m_Context));
+            }
+        }
+    }
+
+    public class ConcreteStateC : State
+    {
+        public ConcreteStateC(Context context) : base(context)
+        {
+        }
+
+        public override void Handle(int value)
+        {
+            Debug.Log("ConcreteStateC.Handle(), value=" + value);
+
+            if (value > 30)
+            {
+                Debug.Log("ConcreteStateC.Handle(), Change To StateA");
+                m_Context.SetState(new ConcreteStateA(m_Context));
+            }
+        }
+    }
+}
+
+
---
+++ Assets/MyBook/Chapter3_State/StateTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace mj
+{
+    public class StateTest : MonoBehaviour
+    {
+        void Start()
+        {
+            Context C = new Context();
+            ConcreteStateA a = new ConcreteStateA(C);
+            C.SetState(a);
+            C.Request(11);
+            C.Request(21);
+            C.Request(31);
+        }
+    }
+}
+
+
